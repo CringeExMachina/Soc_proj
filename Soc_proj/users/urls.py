8df7e293e0a2eafdb18fea6c3348041837aaaafb
Дмитрий <kaneki_soma@bk.ru>
2023-03-28 15:18:18 +0300

    add password template

--- Soc_proj/users/urls.py
+++ Soc_proj/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView,PasswordResetView,PasswordChangeView,PasswordChangeDoneView
 from .import views
 
 app_name='users'
@@ -7,5 +7,13 @@ app_name='users'
 urlpatterns = [
     path('logout/',views.LogOut.as_view(),name='logout'),
     
-    path('signup/',views.SignUp.as_view(),name='signup')
+    path('signup/',views.SignUp.as_view(),name='signup'),
+    
+    path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
+    
+    path('password_reset_form/',PasswordResetView.as_view(template_name='users/password_reset_form.html'),name='password_reset_form'),
+    
+    path('password_change/',PasswordChangeView.as_view(template_name='users/password_change.html'),name='password_change_form'),
+    
+    path('password_change/done/',PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),name='password_change_done'),
 ]
